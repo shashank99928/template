3c3d05c568332cfe3d3d3e4ad4d6c89845ecc1d7
shashank99928 <shashank99928@gmail.com>
2026-05-10 22:07:08 +0530

    feat: integrate Vitest and Playwright test suites with basic unit and E2E coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/setupTests.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/**', '**/e2e/**'],
+  }
 });
